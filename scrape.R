24817d4cc11ba6df68413ebad9a03b759981179c
Courtney Gale Simmons <cgsimmon@wakehealth.edu>
2024-08-12 15:23:52 -0700

    Match all API names

--- scrape.R
+++ scrape.R
@@ -4,7 +4,7 @@ req <- request("https://newsapi.org/v2/everything") |>
     q = '`"data science"`',
     from = Sys.Date() - 1,
     pageSize = 10,
-    apiKey = Sys.getenv("newsAPI")
+    apiKey = Sys.getenv("NEWS_API")
   )
 resp <- req_perform(req)
 resp_body_json(resp)
